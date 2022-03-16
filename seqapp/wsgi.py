a63dafe53e43051a8db60df521918380cfbd2ef3
jxzhou <zjx1996@pku.edu.cn>
2022-03-16 11:19:37 +0800

    start project

---
+++ seqapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for seqapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seqapp.settings')
+
+application = get_wsgi_application()
