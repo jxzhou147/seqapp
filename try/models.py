f999962aa7e8271d0666a42cb036b5b5328b11d4
jxzhou <zjx1996@pku.edu.cn>
2022-03-25 23:20:16 +0800

    Edit views

--- try/models.py
+++ try/models.py
@@ -8,4 +8,3 @@ class Function(models.Model):
 
     def __str__(self):
         return self.function_name
-
--- try/views.py
+++ try/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .functions import sequence_len
 
 
 def index(request):
-    return HttpResponse("My index.")
+    return render(request, 'try/index.html')
+
+
+def results(request):
+    sequence_input = request.POST['sequence']
+    sequence_length = sequence_len(sequence_input)
+    context = {'output': sequence_length}
+    return render(request, 'try/results.html', context)
